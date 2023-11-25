22fcc97d7db91dd0719b1f543ece7eb2b700a1bf
kenhibiki1 <mafiqhakimi52@gmail.com>
2023-11-25 19:17:44 +0800

    Created website, no lists of books yet

--- bookstore/catalog/views.py
+++ bookstore/catalog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'catalog/book_list.html', {'books': books})
