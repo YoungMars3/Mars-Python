78f4cc3ffc9eddfce001c7127f81dfd3979bfaa8
kenhibiki1 <148319723+kenhibiki1@users.noreply.github.com>
2023-12-13 15:22:15 +0800

    polymorphism exercise

--- Class_Work/Phone.py
+++ Class_Work/Phone.py
@@ -1,28 +1,37 @@
 class Hot Drink:
-def ＿init＿(self, name, temperature):
-self.name = name
-self.temperature = temperature
+    def ＿init＿(self, name, temperature):
+        self.name = name
+        self.temperature = temperature
+
 def info(self):
-return f"{self.name} is a hot drink served at {self.temperature}°C"
+    return f"{self.name} is a hot drink served at {self.temperature}°C"
+
 class Coffee(Hot Drink):
-def ＿init＿(self, name, temperature, caffeine_level):
-super().＿init＿(name, temperature)
-self.caffeine_level = caffeine_level
+    def ＿init＿(self, name, temperature, caffeine_level):
+        super().＿init＿(name, temperature)
+        self.caffeine_level = caffeine_level
+
 def boost(self):
-return f"{self.name} gives you a caffeine boost of level {self.caffeine_level}"
+    return f"{self.name} gives you a caffeine boost of level {self.caffeine_level}"
+
 class Tea(Hot Drink):
-def ＿init＿(self, name, temperature, type):
-super().＿init＿(name,
-temperature)
-self.type = type
+    def ＿init＿(self, name, temperature, type):
+        super().＿init＿(name,temperature)
+        self.type = type
+
 def variety(self):
-return f"{self.name} is a type of {self.type} tea" # Creating instances
+    return f"{self.name} is a type of {self.type} tea"
+
+# Creating instances
 coffee = Coffee("Coffee", 85, "High")
 tea = Tea("Tea", 70, "Green")
+
 # Accessing methods
 print( coffee.info ())
+
 # Output: Coffee is a hot drink served at 85°C
 print(coffee.boost())
+
 # Output: Coffee gives you a caffeine boost of level
 High
 print( tea.info ())
