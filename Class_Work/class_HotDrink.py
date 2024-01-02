28295b514a3c265facd541455bb8a8337774f226
Nasif Rahman <nasifr333@gmail.com>
2024-01-02 23:21:21 +0800

    Edited class work for 13.12.23

--- Class_Work/class_HotDrink.py
+++ Class_Work/class_HotDrink.py
@@ -1,32 +1,40 @@
 class HotDrink:
-    def describe_type(self):
-        print("Specify a drink")
-    def describe_temp(self):
-        print("Specify a drink")
-class Coffee:
-    def describe_type(self):
-        print("Espresso")
-    def describe_temp(self):
-        print("Hot")
-class Tea:
-    def describe_type(self):
-        print("Green Tea")
-    def describe_temp(self):
-        print("Warm")
-class HotChocolate:
-    def describe_type(self):
-        print("Dark Chocolate")
-    def describe_temp(self):
-        print("Very Hot")
+    def __init__(self, temperature):
+        self.temperature = temperature
 
-#Create instances and demonstrate polymorphism
-coffee = Coffee()
-tea = Tea()
-hotchocolate = HotChocolate()
+    def describe(self):
+        return 0
 
-coffee.describe_type()
-coffee.describe_temp()
-tea.describe_type()
-tea.describe_temp()
-hotchocolate.describe_type()
-hotchocolate.describe_temp()
+class Coffee(HotDrink):
+    def __init__(self, coffee_type, temperature):
+        super().__init__(temperature)
+        self.coffee_type = coffee_type
+
+    def describe(self):
+        return f"Coffee - Type: {self.coffee_type}, Temperature: {self.temperature}"
+
+class Tea(HotDrink):
+    def __init__(self, tea_type, temperature):
+        super().__init__(temperature)
+        self.tea_type = tea_type
+
+    def describe(self):
+        return f"Tea - Type: {self.tea_type}, Temperature: {self.temperature}"
+
+class HotChocolate(HotDrink):
+    def __init__(self, chocolate_type, temperature):
+        super().__init__(temperature)
+        self.chocolate_type = chocolate_type
+
+    def describe(self):
+        return f"Hot Chocolate - Type: {self.chocolate_type}, Temperature: {self.temperature}"
+
+#Creating instances
+espresso = Coffee("Espresso", "Hot")
+green_tea = Tea("Green Tea", "Warm")
+dark_chocolate = HotChocolate("Dark Chocolate", "Very Hot")
+
+#Accessing methods
+print(espresso.describe())
+print(green_tea.describe())
+print(dark_chocolate.describe())
