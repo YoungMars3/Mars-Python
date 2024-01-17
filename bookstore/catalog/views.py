3be58897b21d03be98e2794ae5231debf9ed89b5
Nasif <148360095+Nasif-ctrl@users.noreply.github.com>
2024-01-17 10:45:34 +0800

    Delete bookstore directory
    Deleting as no longer relevant

--- bookstore/catalog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-def book_list(request):
-    books = Book.objects.all()  # Retrieve all books from the database
-    return render(request, 'catalog/book_list.html', {'books': books})
